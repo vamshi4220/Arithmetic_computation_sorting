4378c51c31def05dbd43b060ae19589f4f9db682
vamshi4220 <ud75281@gmail.com>
2020-10-31 04:35:58 +0530

    added uc3 computation

--- Aerthmaticcomputation.sh
+++ Aerthmaticcomputation.sh
@@ -5,6 +5,7 @@
 read -p "enter three numbers :" num1 num2 num3
 
 #UC2
+echo $((num1+num2*num3))
 
-read -p "enter three numbers :" num1 num2 num3
-echo $((num1+num2+num3))
+#uc3
+echo $((num1*num2+num3))
