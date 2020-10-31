fc098652f19342189ee903b4ec1c69aa53b4d319
vamshi4220 <ud75281@gmail.com>
2020-10-31 08:11:11 +0530

    array sorted

--- Aerthmaticcomputation.sh
+++ Aerthmaticcomputation.sh
@@ -35,3 +35,22 @@ arr[((count++))]="${result[counter]}"
 arr[((count))]="${result[counter]}"
 
 echo "array -" ${result[@]}
+
+#uc8
+for ((i = 0; i<4; i++)) 
+do
+      
+    for((j = 0; j<4-i-1; j++)) 
+    do
+      
+        if [ ${arr[j]} -lt ${arr[$((j+1))]} ] 
+        then 
+            temp=${arr[j]} 
+            arr[$j]=${arr[$((j+1))]}   
+            arr[$((j+1))]=$temp 
+        fi
+    done
+done
+  
+echo "Array in sorted order :"
+echo ${arr[*]} 
